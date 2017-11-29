04c4b0967fa7aa1937362a5e79bc1afde15f54ae
sarah lund <sarah.christine.lund@gmail.com>
2017-11-29 17:29:05 -0500

    added new link for project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,6 +24,12 @@ class Projects extends Component {
       <p id="welcome">This is my project page!
       <br/>Please explore my recent work!</p>
 
+      <p class="projectlink"> <a target="_blank" href="http://candide-beta.surge.sh/">Maison Candide</a></p>
+ <p class="tech">  HTML, Javascript, Shopify SDK, CSS3 </p>
+   <a target="_blank" href="http://candide-beta.surge.sh/"><img class="big" src="https://i.imgur.com/02ANO9R.png"/></a>
+
+
+
 
        <p class="projectlink"> <a target="_blank" href="https://mood-diary-scl.herokuapp.com/home">Mood Diary</a></p>
        <p class="tech">  React.js, IBM Watson API, Firebase </p>
@@ -37,9 +43,9 @@ class Projects extends Component {
   <p class="tech"> HTML5, CSS3, Javascript </p>
   <a target="_blank" href="http://art-aficionado.surge.sh"><img class="big" src="https://i.imgur.com/tVEikzq.png"/></a>
 
-   <p class="projectlink"> <a target="_blank" href="http://boiling-arithmetic.surge.sh/">React Giphy</a></p>
+   <p class="projectlink"> <a target="_blank" href="http://pick-your-own.surge.sh/">React Giphy</a></p>
    <p class="tech"> React.js, Giphy API, CSS3 </p>
-   <a target="_blank" href="http://boiling-arithmetic.surge.sh/"><img class="big" src="https://i.imgur.com/glJ728e.png" alt="giphypic"/></a>
+   <a target="_blank" href="http://pick-your-own.surge.sh/"><img class="big" src="https://i.imgur.com/glJ728e.png" alt="giphypic"/></a>
 
   <p class="projectlink"> <a target="_blank" href="https://bshapiro.surge.sh">Portfolio Site</a></p>
   <p class="tech">  React.js, Javascript, CSS3 </p>
@@ -50,10 +56,6 @@ class Projects extends Component {
    <p class="tech"> React.js, Node.js, Express.js, CSS3 </p>
    <a target="_blank" href="https://scl-tweeder.herokuapp.com/"><img class="big" src="https://i.imgur.com/cIOB3MG.png" alt="tweederpic"/></a>
 
-
- <p class="projectlink"> <a target="_blank" href="http://candide-beta.surge.sh/">Maison Candide (In Development)</a></p>
- <p class="tech">  HTML, Javascript, Shopify SDK, CSS3 </p>
-   <a target="_blank" href="http://candide-beta.surge.sh/"><img class="big" src="https://i.imgur.com/02ANO9R.png"/></a>
 
 
 
